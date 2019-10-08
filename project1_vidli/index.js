4e23f7e8490193044788959d0fe9cca96caf6353
necrosith <necrosith@yandex.ru>
2019-10-08 11:19:25 +0300

    Project Vidly - bugs fixed. Added POST, PUT, GET and DELETE

--- project1_vidli/index.js
+++ project1_vidli/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3001;
 
-const genres = [{
+let genres = [{
         id: 1,
         name: "action"
     },
@@ -25,7 +25,7 @@ const genres = [{
     {
         id: 5,
         name: "detective"
-    },
+    }
 ]
 
 app.listen(port, () => {
@@ -33,15 +33,82 @@ app.listen(port, () => {
 });
 
 app.get('/api/genres', (req, res) => {
-    res.send(req.body);
+    res.send(genres);
 });
 
-app.post();
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(genre => {
+        return genre.id === parseInt(req.params.id);
+    });
 
-app.put();
+    if (!genre) {
+        return res.status(404).send('The genre with the given id could not be found');
+    }
 
-app.delete();
+    res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateInput(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    genres.push(genre);
+    res.send(genre);
+
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(genre => {
+        return genre.id === parseInt(req.params.id);
+    });
+
+    if (!genre) {
+        return res.status(404).send('The genre with the given id could not be found');
+    }
+
+    const { error } = validateInput(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    genre.name = req.body.name;
+    genres.push(genre);
+
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(genre => {
+        return genre.id === parseInt(req.params.id);
+    });
+
+    if (!genre) {
+        return res.status(404).send('The genre with the given id could not be found');
+    }
+
+    const { error } = validateInput(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    };
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+});
+
 
 function validateInput(input) {
+    schema = {
+        name: Joi.string().min(2).required()
+    };
 
+    return Joi.validate(input, schema);
 }
