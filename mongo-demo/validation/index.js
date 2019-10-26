8520f769ebfdcb6b1f89acf4c1c604ab8eb96fc9
Yan_Pustynnyy <necrosith@yandex.ru>
2019-10-26 23:52:47 +0300

    Info on validation added

---
+++ mongo-demo/validation/index.js
@@ -0,0 +1,42 @@
+// Mongoose is an extension for node to handle work with mongdb
+const mongoose = require('mongoose');
+
+// Connect to the db, specifying its name
+mongoose.connect('mongodb://localhost/test')
+    .then(() => console.log(('Conected to MongoDB')))
+    .catch(err => console.log('Something went wrong:', err));
+
+const courseSchema = new mongoose.Schema({
+    name: { type: String, required: true }
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+
+async function createCourse() {
+    const course = new Course({
+        // name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    // If you don't have a valid course data, e.g. course without name
+    // this will catch it
+    try {
+        const result = await course.save();
+        console.log(result);
+
+        // You can also trigger validation manually
+        //await course.validate();
+    } catch (err) {
+        console.log(err.message);
+    }
+
+}
+
+createCourse();
