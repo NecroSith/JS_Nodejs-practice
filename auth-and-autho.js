e624fc17bdf51bf481ba5ca392cb3cbdf1d6874f
Yan_Pustynnyy <necrosith@yandex.ru>
2019-11-22 23:17:29 +0300

    Info on authorization and authentication added

---
+++ auth-and-autho.js
@@ -0,0 +1,8 @@
+// Authentication and authorization
+
+// Authentication - the process of checking that user is really who he claims he is (checking login and pass, sms notif etc)
+
+// Authorization - the process of checking user permissions to do smth
+
+// Register, use to authenticate: POST to /api/users
+// Login, use to authorize: POST to /api/logins
