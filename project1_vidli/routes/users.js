13991a6aa0949fddcb5911a7d7bd1c49064fe02e
Yan_Pustynnyy <necrosith@yandex.ru>
2019-11-29 22:28:06 +0300

    Project Vidly: added user request to get data about self

--- project1_vidli/routes/users.js
+++ project1_vidli/routes/users.js
@@ -1,10 +1,14 @@
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const auth = require('../middleware/auth');
 const _ = require('lodash');
 const { User, validate } = require('../models/users');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async(req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+})
 
 router.post('/', async(req, res) => {
     const { error } = validate(req.body);
