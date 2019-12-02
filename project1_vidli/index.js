634b5d73cbac2e688588f754ac0098d90bc05867
Yan_Pustynnyy <necrosith@yandex.ru>
2019-12-02 23:48:27 +0300

    Project Vidly: aded unhandled rejections error handling

--- project1_vidli/index.js
+++ project1_vidli/index.js
@@ -22,11 +22,43 @@ const auth = require('./routes/auth');
 const error = require('./middleware/error');
 const app = express();
 
+// proces listener to handle errors not located in request handlers
+process.on('uncaughtException', ex => {
+    console.log('Uncaught exception!!!');
+    winston.error(ex.message, ex);
+    // As a best practice we should terminate process
+    process.exit(1);
+});
+
+// Alternatively, we can use winston listener to handle uncaught exceptions
+// and log them to a separate file
+winston.handleExceptions(new winston.transports.File({ filename: 'project1_vidli/uncaughtExceptions.log' }))
+
+// Process listener to handle errors in promises in runtime
+process.on('unhandledRejection', ex => {
+    console.log('Unhandled rejection!!!');
+    winston.error(ex.message, ex);
+    process.exit(1);
+});
+
+//There is no listener for unhandled rejection in winston yet
+// so as a trick we can use this
+process.on('unhandledRejection', ex => {
+    throw (ex); // this will generate uncaught exception which will be caught by the listener
+});
+
 // store logs in file
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+winston.add(new winston.transports.File({ filename: 'project1_vidli/logfile.log' }));
 
 // store logs in mongodb itself
 winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidli' }));
+
+// This error will not be handled by winston by default
+// because it is outside express scope (outside request handlers)
+// throw new Error('Something failed in the startup!');
+
+const p = Promise.reject('Something broke!!');
+p.then(() => console.log('Done');)
 
 if (!config.get("jwtPrivateKey")) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
