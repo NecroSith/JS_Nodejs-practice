0269a4ef782efacf3dee1301614c2212657d37c4
Yan_Pustynnyy <necrosith@yandex.ru>
2019-12-03 21:57:58 +0300

    Project Vidly: Validation refactored

--- project1_vidli/index.js
+++ project1_vidli/index.js
@@ -1,17 +1,14 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
 const express = require('express');
-
-
 const app = express();
 
 require('./startup/logging')();
 require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3001;
-
 app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+    winston.info(`Listening on port ${port}...`);
 });
