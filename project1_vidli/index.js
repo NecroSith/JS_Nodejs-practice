9ae94b87bf343aa02e3b67a6f99b39e3ff7fef4d
Yan_Pustynnyy <necrosith@yandex.ru>
2019-12-03 21:53:22 +0300

    Project Vidly: Configs refactored

--- project1_vidli/index.js
+++ project1_vidli/index.js
@@ -1,4 +1,3 @@
-const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -7,26 +6,9 @@ const express = require('express');
 const app = express();
 
 require('./startup/logging')();
+require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-
-
-// This error will not be handled by winston by default
-// because it is outside express scope (outside request handlers)
-// throw new Error('Something failed in the startup!');
-
-// const p = Promise.reject('Something broke!!');
-// p.then(() => console.log('Done'))
-
-if (!config.get("jwtPrivateKey")) {
-    console.error('FATAL ERROR: jwtPrivateKey is not defined');
-
-    // Closes the process of the app
-    // anything except 0 means error so I used 1 here
-    process.exit(1);
-}
-
 
 const port = process.env.PORT || 3001;
 
---
+++ project1_vidli/startup/config.js
@@ -0,0 +1,20 @@
+const config = require('config');
+
+module.exports = function() {
+    // This error will not be handled by winston by default
+    // because it is outside express scope (outside request handlers)
+    // throw new Error('Something failed in the startup!');
+
+    // const p = Promise.reject('Something broke!!');
+    // p.then(() => console.log('Done'))
+
+    if (!config.get("jwtPrivateKey")) {
+        // as a best practice make error messages object
+        // if you return a string you can't have a stacktrace in error handler
+        throw new Error({ 'FATAL ERROR: jwtPrivateKey is not defined' });
+
+        // Closes the process of the app
+        // anything except 0 means error so I used 1 here
+        // process.exit(1);
+    }
+}
