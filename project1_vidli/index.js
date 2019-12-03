e01fa9ee8c5e77b5029eab816f205f53410d072b
Yan_Pustynnyy <necrosith@yandex.ru>
2019-12-03 21:46:50 +0300

    Project Vidly: Logging refactored

--- project1_vidli/index.js
+++ project1_vidli/index.js
@@ -1,11 +1,3 @@
-// Before all other routers error handling middleware should be declared
-// so it can wrap each request in error handler during runtime
-// therefore there is no need to do anything for the dev
-require('express-async-errors');
-
-const winston = require('winston');
-require('winston-mongodb');
-
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -14,39 +6,11 @@ const express = require('express');
 
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-// process listener to handle errors not located in request handlers
-process.on('uncaughtException', ex => {
-    console.log('Uncaught exception!!!');
-    winston.error(ex.message, ex);
-    // As a best practice we should terminate process
-    process.exit(1);
-});
 
-// Alternatively, we can use winston listener to handle uncaught exceptions
-// and log them to a separate file
-winston.handleExceptions(new winston.transports.File({ filename: 'project1_vidli/uncaughtExceptions.log' }))
-
-// Process listener to handle errors in promises in runtime
-process.on('unhandledRejection', ex => {
-    console.log('Unhandled rejection!!!');
-    winston.error(ex.message, ex);
-    process.exit(1);
-});
-
-//There is no listener for unhandled rejection in winston yet
-// so as a trick we can use this
-process.on('unhandledRejection', ex => {
-    throw (ex); // this will generate uncaught exception which will be caught by the listener
-});
-
-// store logs in file
-winston.add(new winston.transports.File({ filename: 'project1_vidli/logfile.log' }));
-
-// store logs in mongodb itself
-winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/vidli' }));
 
 // This error will not be handled by winston by default
 // because it is outside express scope (outside request handlers)
