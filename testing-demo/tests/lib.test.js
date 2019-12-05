cf9767fb9c4644b9c6911d7049158ac1b3a85b92
Yan_Pustynnyy <necrosith@yandex.ru>
2019-12-05 23:37:47 +0300

    Function for test string refactored

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require('../lib');
 
-test('absolute - should return a positive number in the input is positive', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-});
+describe('absolute', () => {
+    it('should return a positive number in the input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
 
-test('absolute - should return a positive number in the input is negative', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
+    it('should return a positive number in the input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
 
-test('absolute - should return a 0 in the input is 0', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
-});
+    it('should return a 0 in the input is 0', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+})
