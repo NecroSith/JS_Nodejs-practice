5ca0033047aa3ae0a305e2ceac6229d8fdb90aeb
Yan_Pustynnyy <necrosith@yandex.ru>
2019-12-05 23:29:27 +0300

    Added test for strings

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,3 +1,16 @@
-test('First test', () => {
+const lib = require('../lib');
 
+test('absolute - should return a positive number in the input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number in the input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return a 0 in the input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
 });
