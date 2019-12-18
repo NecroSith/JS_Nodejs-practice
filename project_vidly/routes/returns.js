9389ab3e2c620f1a3d8bda77a818273accdb39be
NecroSith <necrosith@yandex.ru>
2019-12-18 23:19:16 +0300

    Project Vidly: code refactored and test for checing if the rental is not found added

--- project_vidly/routes/returns.js
+++ project_vidly/routes/returns.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { Rental } = require('../models/rentals');
 const router = express.Router();
 
 router.post('/', async(req, res) => {
     if (!req.body.customerId) return res.status(400).send('CustomerId not provided');
     if (!req.body.movieId) return res.status(400).send('MovieId not provided');
+
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
+    });
+
+    if (!rental) return res.status(404).send('Rental not found');
+
     return res.status(401).send('Unauthorized');
 });
 
